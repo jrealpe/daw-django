1b293e37b2b51fa8c3b1a127687e07909955b622
Julio Realpe <jrealpe94@gmail.com>
2017-02-18 08:58:44 -0500

    [ADD] models to admin

--- i_site/admin.py
+++ i_site/admin.py
@@ -1,3 +1,35 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+
+class RequirementInline(admin.TabularInline):
+    model = Requirement
+    extra = 1
+
+
+class ScheduleInline(admin.TabularInline):
+    model = Schedule
+    extra = 1
+
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+    inlines = [RequirementInline,]
+    list_display = ('name', 'description', 'date_created', 'date_last_updated', 'is_activated')
+    list_display_links = ('name', 'description')
+
+
+@admin.register(W2W)
+class W2WAdmin(admin.ModelAdmin):
+    list_display = ('name', 'course', 'date_created', 'date_last_updated', 'is_activated')
+
+
+@admin.register(Staff)
+class StaffAdmin(admin.ModelAdmin):
+    inlines = [ScheduleInline,]
+    list_display = ('name', 'course', 'phone', 'email', 'charge', 'is_activated')
+
+
+@admin.register(News)
+class NewsAdmin(admin.ModelAdmin):
+    list_display = ('name', 'description', 'date_created', 'date_last_updated', 'is_activated')
